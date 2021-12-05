584e4cb6cc0908f33002fb952817972baa067abe
volkov-developer <sergey.volkov.freelancer@gmail.com>
2021-12-05 11:05:39 +0200

    UI interactivity completed

--- src/init.js
+++ src/init.js
@@ -1,9 +1,10 @@
 import game from "./gameState.js";
-
-const TICK_RATE = 3000;
+import { TICK_RATE } from "./constants.js";
+import initButtons from "./buttons.js";
 
 async function init() {
   console.log('starting game');
+  initButtons(game.handleUserAction);
 
   let nextTimeToTick = Date.now();
 
